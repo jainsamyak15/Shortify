b2af082ef3bb78997929b6b79bc942d086843c8c
gulshanpr <gulshankumarprasad04@gmail.com>
2025-02-15 19:52:16 +0530

    db setup

--- README.md
+++
@@ -1,6 +0,0 @@
-## TODO 
-
-- Add verification logic in src/action/verify.ts (done)
-- Add database url , database updation , wallet connect , login , and minting logic 
-- add verification agent url 
-# Shortify
--- src/actions/submit.ts
+++ src/actions/submit.ts
@@ -1,29 +1,30 @@
-"use server"
-import prisma from "@/db/prisma"
+"use server";
+import prisma from "../db/prisma";
 
+export default async function SubNews(
+  title: string,
+  description: string,
+  score: number
+) {
+  // pushing to database
 
-export default async function SubNews(title: string, description: string, score: number){
-    
-    // pushing to database 
-   
-    const response = await prisma.news.create({
-        data : {
-         title : title,
-         description: description,
-         author : "fetch from middleware",
-         trustScore : score,
-         mint_price : 0 // replace later
-         }
-    })
+  const response = await prisma.news.create({
+    data: {
+      title: title,
+      description: description,
+      author: "fetch from middleware",
+      trustScore: score,
+      mint_price: 0, // replace later
+    },
+  });
 
-    const resID =  response.id
-    
-    return resID;
+  const resID = response.id;
 
+  return resID;
 }
 
 // id String @unique @default(cuid())
-// title String 
+// title String
 // description String
 // author String
 // trustScore Int
